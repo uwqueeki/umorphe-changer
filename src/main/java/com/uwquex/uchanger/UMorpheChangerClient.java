864c356aefc3bc0bf2fb754edea0c912b6f3fd72
uwqueeki <n3st.l4mNn7u-kvx2S00r_ell7.kffx@proton.me>
2026-03-22 19:55:03 +0000

    Add files via upload

---
+++ src/main/java/com/uwquex/uchanger/UMorpheChangerClient.java
@@ -0,0 +1,10 @@
+package com.uwquex.uchanger;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class UMorpheChangerClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
